e23c1abd9df469a554581351edafc890412f4cdf
danaa <danaalayoubi03@gmail.com>
2024-12-04 15:44:07 +0300

    init for setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rafiq_app/screens/Login_screen.dart';
 import 'package:rafiq_app/theme.dart';
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
